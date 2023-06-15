98bf3cc406df93ddc57be372a4917c42d4ec7bc5
mhh <mike.hukiewitz@robotter.ai>
2023-06-15 14:54:08 +0200

    fix node module building and mount to /opt/node_modules

--- dependency_builder/build.py
+++ dependency_builder/build.py
@@ -43,8 +43,8 @@ async def build_and_upload_node_modules(
     modules: List[str],
 ) -> CID:
     root_modules = Path(
-        "/root/.node_modules"
-    )  # /root/.node_modules is by default searched by NodeJS for modules running as root
+        "/opt/node_modules"
+    )  # /opt/node_modules is by default the NODE_PATH env on AlephVMs
     # check if directory exists, clean if necessary
     if not root_modules.exists():
         root_modules.mkdir(parents=True, exist_ok=True)
@@ -57,11 +57,11 @@ async def build_and_upload_node_modules(
     mksquashfs_dir = Path("/opt/sqashfs/")
     mksquashfs_dir.mkdir(parents=True, exist_ok=True)
     squashfs_path = mksquashfs_dir / Path(f"{modules_hash}.squashfs")
-    await run_subprocess(f"npm install --prefix {str(root_modules)} {' '.join(modules)}")
-    await run_subprocess(f"mv {str(root_modules)}/node_modules/* {str(root_modules)}")
-    await run_subprocess(f"rm -rf {str(root_modules)}/node_modules")
+    await run_subprocess(f"npm install -g {' '.join(modules)}")
+    await run_subprocess(f"mv /usr/local/lib/node_modules {str(root_modules)}")
     await run_subprocess(f"mksquashfs {str(root_modules)} {squashfs_path}")
-    (_, cid) = await asyncio.gather(
+    (_, _, cid) = await asyncio.gather(
+        run_subprocess(f"rm -rf /usr/local/lib/node_modules"),
         run_subprocess(f"rm -rf {str(root_modules)}"),
         upload_sources(Path(squashfs_path)),
     )
