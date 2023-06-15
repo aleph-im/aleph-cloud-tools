4001594cc04c8bc5aad0b3f86f1808a5835d55f1
mhh <mike.hukiewitz@robotter.ai>
2023-06-15 14:17:51 +0200

    add node.js support

--- dependency_builder/build.py
+++ dependency_builder/build.py
@@ -12,12 +12,11 @@ from utils import (
 
 
 async def build_and_upload_requirements_python(
-    # session: AuthenticatedAlephClient,
     requirements: List[str],
 ) -> CID:
     opt_packages = Path(
         "/opt/packages"
-    )  # /opt/packages is by default imported into Python
+    )  # /opt/packages is by default imported into Aleph VMs
     # check if directory exists, clean if necessary
     if not opt_packages.exists():
         opt_packages.mkdir(parents=True, exist_ok=True)
@@ -27,17 +26,7 @@ async def build_and_upload_requirements_python(
 
     requirements_hash = make_requirements_hash(requirements)
 
-    # check if requirements are already uploaded
-    # resp = await session.get_messages(
-    #    message_type=MessageType.store,
-    #    tags=[requirements_hash],
-    #    channels=[SOURCES_CHANNEL],
-    # )
-    # if resp.messages:
-    #    return resp.messages[0]
-
-    # install requirements, dependency_builder & upload squashfs, clean up
-    mksquashfs_dir = Path("/opt/mysqashfs/")
+    mksquashfs_dir = Path(f"/opt/sqashfs/")
     mksquashfs_dir.mkdir(parents=True, exist_ok=True)
     squashfs_path = mksquashfs_dir / Path(f"{requirements_hash}.squashfs")
     await run_subprocess(f"pip install -t {str(opt_packages)} {' '.join(requirements)}")
@@ -45,15 +34,36 @@ async def build_and_upload_requirements_python(
     (_, cid) = await asyncio.gather(
         run_subprocess(f"rm -rf {str(opt_packages)}"),
         upload_sources(Path(squashfs_path)),
-        # upload_sources(session, Path(squashfs_path), tags=[requirements_hash]),
     )
     await run_subprocess(f"rm -rf {squashfs_path}")
     return cid
 
 
-async def build_and_upload_node_dependencies(
-    # session: AuthenticatedAlephClient,
-    package_json: str,
+async def build_and_upload_node_modules(
+    modules: List[str],
 ) -> CID:
-    # TODO: implement
-    raise NotImplementedError
+    root_modules = Path(
+        "/root/.node_modules"
+    )  # /root/.node_modules is by default searched by NodeJS for modules running as root
+    # check if directory exists, clean if necessary
+    if not root_modules.exists():
+        root_modules.mkdir(parents=True, exist_ok=True)
+    else:
+        shutil.rmtree(root_modules)
+        root_modules.mkdir(parents=True, exist_ok=True)
+
+    modules_hash = make_requirements_hash(modules)
+
+    mksquashfs_dir = Path("/opt/sqashfs/")
+    mksquashfs_dir.mkdir(parents=True, exist_ok=True)
+    squashfs_path = mksquashfs_dir / Path(f"{modules_hash}.squashfs")
+    await run_subprocess(f"npm install --prefix {str(root_modules)} {' '.join(modules)}")
+    await run_subprocess(f"mv {str(root_modules)}/node_modules/* {str(root_modules)}")
+    await run_subprocess(f"rm -rf {str(root_modules)}/node_modules")
+    await run_subprocess(f"mksquashfs {str(root_modules)} {squashfs_path}")
+    (_, cid) = await asyncio.gather(
+        run_subprocess(f"rm -rf {str(root_modules)}"),
+        upload_sources(Path(squashfs_path)),
+    )
+    await run_subprocess(f"rm -rf {squashfs_path}")
+    return cid
--- dependency_builder/main.py
+++ dependency_builder/main.py
@@ -2,6 +2,7 @@
 This is the Aleph Console Backend VM. Its current primary purpose is to accept a list of python or node.js dependencies
 and to dependency_builder the according immutable IPFS volume.
 """
+import json
 import logging
 from typing import List
 
@@ -9,7 +10,7 @@ from aleph.sdk.vm.app import AlephApp
 from fastapi import FastAPI, UploadFile, File
 from starlette.middleware.cors import CORSMiddleware
 
-from build import build_and_upload_requirements_python
+from build import build_and_upload_requirements_python, build_and_upload_node_modules
 from utils import CID
 
 logger = (
@@ -42,7 +43,6 @@ async def health() -> dict:
 @app.post("/build/python3.9")
 async def build_python3_9(requirements: List[str]) -> CID:
     """Build a python 3.9 environment."""
-    # aleph_client = AuthenticatedAlephClient(get_fallback_account(), settings.API_HOST)
     return await build_and_upload_requirements_python(requirements)
 
 
@@ -51,7 +51,22 @@ async def build_python3_9_requirements(
     data_file: UploadFile = File(...),
 ) -> CID:
     """Build a python 3.9 environment from a requirements.txt file."""
-    # aleph_client = AuthenticatedAlephClient(get_fallback_account(), settings.API_HOST)
     requirements = data_file.file.read().decode("utf-8").split("\n")
     requirements = [r.strip() for r in requirements if r]
     return await build_and_upload_requirements_python(requirements)
+
+
+@app.post("/build/nodejs")
+async def build_nodejs(modules: List[str]) -> CID:
+    """Build a node.js environment."""
+    return await build_and_upload_node_modules(modules)
+
+
+@app.post("/build/nodejs/package")
+async def build_nodejs_package(
+    data_file: UploadFile = File(...),
+) -> CID:
+    """Build a node.js environment from a package.json file."""
+    modules = json.loads(data_file.file.read().decode("utf-8")).get("dependencies", [])
+    modules = [f"{m}@{v}" for m, v in modules.items()]
+    return await build_and_upload_node_modules(modules)
